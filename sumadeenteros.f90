153b0214c4d221da73b1b7f5c73ec75821893926
camilohuertas <120580234+camilohuertas02@users.noreply.github.com>
2023-12-11 08:43:16 -0500

    Delete sumadeenteros.f90

--- sumadeenteros.f90
+++
@@ -1,9 +0,0 @@
-program holamundo
-        INTEGER a,b,c
-        write (*,*) 'Hola, este scrip suma 2 enteros' 
-        READ (*,*)  a,b
-        c=a+b
-        write (*,*) 'La suma de',a,'y',b,'es',c
-end program holamundo
-
-
